eoghanmurray/lookup-irish

commit 4b90200f9ae6cc88b1ec72ff1482e69a2a8e28da

More aggressive in filling in Noun gender/plurals for words that have only a single sense

teanglann.py
@@ -245,50 +245,6 @@ def get_teanglann_senses(
                         if verbose:
                             print(f'{label}[{formatted_text}]')
 
-                if 'Verb' in types:
-                    vn = assign_verbal_noun(word)
-                    if sense.get('verbal-noun', vn) != vn:
-                        manual_debug()
-                    if vn:
-                        # http://nualeargais.ie/gnag/verbnom1.htm
-                        vnvts = [
-                            'ag ' + vn,
-                            'a ' + lenite(vn),
-                            'le ' + vn,
-                            'do mo ' + lenite(vn),
-                            'do do ' + lenite(vn),
-                            'á ' + vn,
-                            'á ' + lenite(vn),
-                            'dár ' + eclipse(vn),
-                            'do bhur ' + eclipse(vn),
-                            'á ' + eclipse(vn),
-                        ]
-                        vn_examples = []
-                        for vt in vnvts:
-                            if vt in raw_text:
-                                vn_examples.append((raw_text.count(vt), vt))
-                        if vn_examples:
-                            vn_examples.sort(reverse=True)
-                            sense['verbal-noun-examples'] = [vte[1] for vte in vn_examples]
-                        else:
-                            sense['verbal-noun-examples'] = ['ag ' + vn]
-                    sense['verbal-noun'] = vn
-
-                if 'Noun' in types:
-                    gp = format_declensions(plural_genitive, gender, format)
-                    if sense.get('genitive-plural', gp) != gp:
-                        senses.append({
-                            'definitions': [],
-                            'raw_definitions': [],
-                            'gender': gender,
-                            'type-sig': type_sig,
-                            'types': types.copy(),
-                            'pos': pos,
-                        })
-                        sense = senses[-1]
-                    sense['genitive-plural'] = gp
-                    sense['genitive-plural-raw'] = plural_genitive
-
                 if definition and definition not in sense['definitions']:
                     # could filter/rearrange existing definitions here
                     if 'Prefix' in types:
@@ -306,6 +262,52 @@ def get_teanglann_senses(
                             )
                     else:
                         sense['definitions'].append(definition)
+
+
+            if 'Verb' in types:
+                vn = assign_verbal_noun(word)
+                if sense.get('verbal-noun', vn) != vn:
+                    manual_debug()
+                if vn:
+                    # http://nualeargais.ie/gnag/verbnom1.htm
+                    vnvts = [
+                        'ag ' + vn,
+                        'a ' + lenite(vn),
+                        'le ' + vn,
+                        'do mo ' + lenite(vn),
+                        'do do ' + lenite(vn),
+                        'á ' + vn,
+                        'á ' + lenite(vn),
+                        'dár ' + eclipse(vn),
+                        'do bhur ' + eclipse(vn),
+                        'á ' + eclipse(vn),
+                    ]
+                    vn_examples = []
+                    for vt in vnvts:
+                        if vt in raw_text:
+                            vn_examples.append((raw_text.count(vt), vt))
+                    if vn_examples:
+                        vn_examples.sort(reverse=True)
+                        sense['verbal-noun-examples'] = [vte[1] for vte in vn_examples]
+                    else:
+                        sense['verbal-noun-examples'] = ['ag ' + vn]
+                sense['verbal-noun'] = vn
+
+            if 'Noun' in types:
+                gp = format_declensions(plural_genitive, gender, format)
+                if sense.get('genitive-plural', gp) != gp:
+                    senses.append({
+                        'definitions': [],
+                        'raw_definitions': [],
+                        'gender': gender,
+                        'type-sig': type_sig,
+                        'types': types.copy(),
+                        'pos': pos,
+                    })
+                    sense = senses[-1]
+                sense['genitive-plural'] = gp
+                sense['genitive-plural-raw'] = plural_genitive
+
 
     if sort_by_foclóir:
         for sense in senses:

tests.py
@@ -113,3 +113,6 @@ if __name__ == '__main__':
 
     # bad markup; double '(gs. (gs.'
     get_teanglann_definition('colúnaí')
+
+    # no definition, still want to pull back Noun variants
+    get_teanglann_definition('meangadh')

populate_google_sheet.py
@@ -214,11 +214,14 @@ def populate_meta(limit=-1, start_row=2, single_GA=None):
                             if not row.EN or sd in row.EN:
                                 use_sense = True
                     if not use_sense:
-                        if len(senses) == 1 and \
-                           'types' in sense and \
-                           sense['types'].keys() == {row.PoS} and \
-                           (not row.Gender or
-                            sense['gender'] == row.Gender):
+                        if (len(senses) == 1 and
+                            (not row.PoS or (
+                                'types' in sense and
+                                sense['types'].keys() == {row.PoS})
+                            ) and (
+                                not row.Gender or
+                                sense['gender'] == row.Gender
+                            )):
                             use_sense = True
                     if not use_sense:
                         continue
@@ -237,7 +240,7 @@ def populate_meta(limit=-1, start_row=2, single_GA=None):
                             genitive_vn_soup.append(inf)
                     if sense['gender'] and \
                        'Noun' in sense['types'] and \
-                       'Noun' in row.PoS:
+                       ('Noun' in row.PoS or not row.PoS):
                         if sense['gender'] not in genders:
                             genders.append(sense['gender'])
                         if sense.get('genitive-plural', None):
@@ -478,6 +481,12 @@ arg(
     default=False
 )
 
+arg(
+    '--force-noun',
+    type=int,
+    help='''Populate GenitiveVN column even if we don't find a matching sense''',
+    default=2
+)
 
 if __name__ == '__main__':
     args = vars(parser.parse_args())

commit 763d15abf1dacf4f5d2d8bab2999b5c23458038b

Fix bug introduced in 4b90200f9ae6cc88b1ec7 where sense['definition'] was already filled before split because of different genitivevn

teanglann.py
@@ -154,6 +154,55 @@ def get_teanglann_senses(
             print()
             print(f'{Back.RED}{word}'
                   f'{Style.RESET_ALL}', join_parts_of_speech(types), gender)
+
+        if 'Verb' in types:
+            vn = assign_verbal_noun(word)
+            if sense.get('verbal-noun', vn) != vn:
+                manual_debug()
+            if vn:
+                # http://nualeargais.ie/gnag/verbnom1.htm
+                vnvts = [
+                    'ag ' + vn,
+                    'a ' + lenite(vn),
+                    'le ' + vn,
+                    'do mo ' + lenite(vn),
+                    'do do ' + lenite(vn),
+                    'á ' + vn,
+                    'á ' + lenite(vn),
+                    'dár ' + eclipse(vn),
+                    'do bhur ' + eclipse(vn),
+                    'á ' + eclipse(vn),
+                ]
+                vn_examples = []
+                for vt in vnvts:
+                    if vt in raw_text:
+                        vn_examples.append((raw_text.count(vt), vt))
+                if vn_examples:
+                    vn_examples.sort(reverse=True)
+                    sense['verbal-noun-examples'] = [vte[1] for vte in vn_examples]
+                else:
+                    sense['verbal-noun-examples'] = ['ag ' + vn]
+            sense['verbal-noun'] = vn
+
+        if 'Noun' in types:
+            plural_genitive = sense_assign_plural_genitive(
+                word, first_line, gender
+            )
+            gp = format_declensions(word, plural_genitive, gender, format)
+            if sense.get('genitive-plural', gp) != gp:
+                senses.append({
+                    'definitions': [],
+                    'raw_definitions': [],
+                    'gender': gender,
+                    'type-sig': type_sig,
+                    'types': types.copy(),
+                    'pos': pos,
+                })
+                sense = senses[-1]
+            sense['genitive-plural'] = gp
+            sense['genitive-plural-raw'] = plural_genitive
+
+
         for label, subentry in zip(subentry_labels, subentries):
             transs = subentry.find_all(class_='trans')
             if len(transs) > 1 and (
@@ -279,55 +328,6 @@ def get_teanglann_senses(
                             )
                     else:
                         sense['definitions'].append(definition)
-
-
-            if 'Verb' in types:
-                vn = assign_verbal_noun(word)
-                if sense.get('verbal-noun', vn) != vn:
-                    manual_debug()
-                if vn:
-                    # http://nualeargais.ie/gnag/verbnom1.htm
-                    vnvts = [
-                        'ag ' + vn,
-                        'a ' + lenite(vn),
-                        'le ' + vn,
-                        'do mo ' + lenite(vn),
-                        'do do ' + lenite(vn),
-                        'á ' + vn,
-                        'á ' + lenite(vn),
-                        'dár ' + eclipse(vn),
-                        'do bhur ' + eclipse(vn),
-                        'á ' + eclipse(vn),
-                    ]
-                    vn_examples = []
-                    for vt in vnvts:
-                        if vt in raw_text:
-                            vn_examples.append((raw_text.count(vt), vt))
-                    if vn_examples:
-                        vn_examples.sort(reverse=True)
-                        sense['verbal-noun-examples'] = [vte[1] for vte in vn_examples]
-                    else:
-                        sense['verbal-noun-examples'] = ['ag ' + vn]
-                sense['verbal-noun'] = vn
-
-            if 'Noun' in types:
-                plural_genitive = sense_assign_plural_genitive(
-                    word, first_line, gender
-                )
-                gp = format_declensions(word, plural_genitive, gender, format)
-                if sense.get('genitive-plural', gp) != gp:
-                    senses.append({
-                        'definitions': [],
-                        'raw_definitions': [],
-                        'gender': gender,
-                        'type-sig': type_sig,
-                        'types': types.copy(),
-                        'pos': pos,
-                    })
-                    sense = senses[-1]
-                sense['genitive-plural'] = gp
-                sense['genitive-plural-raw'] = plural_genitive
-
 
     if sort_by_foclóir:
         for sense in senses:

tests.py
@@ -122,6 +122,9 @@ if __name__ == '__main__':
     # no definition, still want to pull back Noun variants
     get_teanglann_definition('meangadh')
 
+    # separate senses with separate genitivevn ('journal' has irisí as plural but 'belief' does not)
+    get_teanglann_definition('iris')
+
     # don't use variant plural breitheamhain
     assign_plural_genitive('breitheamh')
 